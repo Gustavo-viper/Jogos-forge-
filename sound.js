1c409c8ff3e314f0d56220504202cfd6ce146887
Gustavo-viper <studioailvatattoo@gmail.com>
2026-09-21 20:50:32 -0300

    Increase master gain value to 5.0

--- sound.js
+++ sound.js
@@ -17,7 +17,7 @@
 
     audioContext = audioContext || new AudioCtx();
     master = audioContext.createGain();
-    master.gain.value = 0.035;
+    master.gain.value = 5.0;
     master.connect(audioContext.destination);
 
     const tones = [55, 82.41, 110];
